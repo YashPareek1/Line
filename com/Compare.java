39966bcbefaec8d56e8a2398a3de457b75f85a5a
YashPareek1 <yashpareekg4@gmail.com>
2021-05-10 00:18:18 +0530

    Length of Line

--- com/Compare.java
+++ com/Compare.java
@@ -1,7 +1,24 @@
 package com;
 
+import java.util.Scanner;
+
 public class Compare {
 public static void main( String[] args) {
 	System.out.println("Hello Line Compare");
+	
+	 Scanner s = new Scanner(System.in);
+     //Getting values from user
+     System.out.println("Enter the first co-ordinate of x");
+     int x1 = s.nextInt();
+     System.out.println("Enter the first co-ordinate of y");
+     int y1 = s.nextInt();
+     System.out.println("Enter the second co-ordinate of x");
+     int x2 = s.nextInt();
+     System.out.println("Enter the second co-ordinate of y");
+     int y2 = s.nextInt();
+     //equation to get solved
+     double line_length = Math.sqrt((Math.pow((x2 - x1) , 2 )+Math.pow((y2 - y1) , 2 )));
+     //Printing the length of line
+     System.out.println("The length of line is" +line_length);
 }
 }
